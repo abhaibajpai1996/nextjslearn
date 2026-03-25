cd02bdcc21b5d09d21679d6fced7d71ff962099a
abajpai36 <abhaibajpai1996@gmail.com>
2026-03-25 12:17:57 +0530

    private methods and route grouping

---
+++ app/_private/page.tsx
@@ -0,0 +1,6 @@
+export default function PrivateF(){
+    return <h1>This is a private page...</h1>
+}
+
+//underscore in the folder name makes the page private and not accessible to users. It can be used for components or any other files which 
+// are not meant to be accessed by users.   
